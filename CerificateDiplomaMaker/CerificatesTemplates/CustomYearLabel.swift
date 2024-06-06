8929eb2310390116f4e3a110bc6d3b1f90df51e1
Sourabh <sourabh.shrivastava20@gmail.com>
2024-06-06 19:00:17 +0530

    create dynamic year update name

--- CerificateDiplomaMaker/CerificatesTemplates/CustomYearLabel.swift
+++ CerificateDiplomaMaker/CerificatesTemplates/CustomYearLabel.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2024 Mobiona. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CustomYearLabel: UILabel {
+    
+    //MARK: - Methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let currentDate = Date()
+        let calendar = Calendar.current
+        let currentYear = calendar.component(. year, from: currentDate)
+        text = "© " + currentYear.description
+    }
+    
+    
+}
